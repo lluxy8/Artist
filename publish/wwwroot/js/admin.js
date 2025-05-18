d561e93770f27ba736b8610d9b54288d987b6b80
muratcan sakin <lluxysa@gmail.com>
2025-05-18 21:12:15 +0300

    publish

---
+++ publish/wwwroot/js/admin.js
@@ -0,0 +1,10 @@
+﻿function slugify(text) {
+    return text.toString().toLowerCase()
+        .replace(/ç/g, 'c').replace(/ğ/g, 'g')
+        .replace(/ı/g, 'i').replace(/ö/g, 'o')
+        .replace(/ş/g, 's').replace(/ü/g, 'u')
+        .replace(/[^a-z0-9\s-]/g, '') // özel karakterleri sil
+        .replace(/\s+/g, '-')        // boşlukları tire yap
+        .replace(/-+/g, '-')         // ardışık tireleri tek tire yap
+        .replace(/^-+|-+$/g, '');    // baştaki ve sondaki tireleri kaldır
+}
