1d196bf6246d7685c43e13d04817ecc53ad80778
muratcan sakin <lluxysa@gmail.com>
2025-05-19 15:04:33 +0300

    publish

--- src/Web/Models/FooterViewModel.cs
+++ src/Web/Models/FooterViewModel.cs
@@ -5,8 +5,8 @@ namespace Web.Models
     public class FooterViewModel
     {
         public Setting Settings { get; set; }
-        public List<Social> Socials { get; set; }
-        public List<Category> Categories { get; set; }
-        public List<Project> Projects { get; set; }
+        public List<Social> Socials { get; set; } = [];
+        public List<Category> Categories { get; set; } = [];
+        public List<Project> Projects { get; set; } = [];
     }
 }
